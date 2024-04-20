1aa3c0036aa6d6d573621222d610fc688a421f9a
nurjahon0920 <n20090920@gmail.com>
2024-04-20 07:22:49 +0500

    my commit

---
+++ main.js
@@ -0,0 +1,65 @@
+//////Begin-1/////////
+let a = prompt("a ning qiymatini kiriting");
+let b = prompt("b ning qiymatini kiriting");
+let S = a * b;
+let P = 2 * (a + b);
+console.log(`Yuzi: ${S}`);
+console.log(`Perimetri: ${P}`);
+/*
+let tomon = prompt("kvadrat tomonini kiriting");
+let p = 4 * tomon;
+console.log(`kvadratning perimetri: ${p}`);
+*/
+////////////Begin-2//////
+// let d = prompt("Aylananing diametrini kiriting");
+// let PI = 3.14;
+// let L = PI * d;
+// console.log(`Aylananing uzunligi: ${L}`);
+
+/*
+let a = prompt("kvadrat tomonining qiymatini kiriting");
+let S = a ** 2;
+console.log(S);
+.
+.
+.
+*/
+
+////////////Begin-3//////
+/*
+let R = prompt("Radius qiymatini kiriting");
+let PI = 3.14;
+let L = 2 * R * PI;
+let S = PI * R ** 2;
+console.log(`Yuzi: ${S}`);
+console.log(`Uzunligi: ${L}`);
+.
+.
+.
+*/
+// let R = prompt("Radius qiymatini kiriting");
+
+////////////Begin-4//////
+// let a = prompt("a_ning qiymatinini kiriting");
+// let b = prompt("b_ning qiymatinini kiriting");
+// let c = a * b;
+// let G = c ** 1 / 2;
+// console.log(`ikki sonning o'rta geometrigi: ${G}`);
+
+////////////Begin-5//////
+// let x = prompt("x_ning qiymatinini kiriting");
+// let y = 3 * x ** 6 - 6 * x ** 2 - 7;
+// console.log(y);
+////////////Begin-6//////
+// let a = prompt("a_ning qiymatinini kiriting");
+// let b = prompt("b_ning qiymatinini kiriting");
+// let c = a;
+// a = b;
+// b = c;
+// console.log(`a = ${a}`);
+// console.log(`b = ${b}`);
+
+////////////Begin-7//////
+////////////Begin-8//////
+////////////Begin-9//////
+////////////Begin-10//////
---
+++ readme.md
@@ -0,0 +1,12 @@
+Type of variables
+var
+let
+const
+
+- Declaration - e’lon qilish -- Hech narsasi yo'q ,bo'sh quti
+- Initialize - ishga tushirish -- declare dagi qutiga qiymat berish
+- Assign - o’zlashtirish -- berilgan qiymatni o'zgartirish
+
+declorationdagi o'zgaruvchilar -- undefined bo'lib turadi
+
+output : alert(), console.log()
