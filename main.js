d44d89aebc185264236973b7ef1c4904a5e99d5d
nurjahon0920 <n20090920@gmail.com>
2024-04-21 19:35:38 +0500

    second commit

--- main.js
+++ main.js
@@ -1,65 +1,82 @@
 //////Begin-1/////////
+/*
 let a = prompt("a ning qiymatini kiriting");
 let b = prompt("b ning qiymatini kiriting");
 let S = a * b;
 let P = 2 * (a + b);
 console.log(`Yuzi: ${S}`);
-console.log(`Perimetri: ${P}`);
-/*
-let tomon = prompt("kvadrat tomonini kiriting");
-let p = 4 * tomon;
-console.log(`kvadratning perimetri: ${p}`);
+    console.log(`Perimetri: ${P}`);
 */
 ////////////Begin-2//////
 // let d = prompt("Aylananing diametrini kiriting");
 // let PI = 3.14;
-// let L = PI * d;
+// let V = PI * d;
 // console.log(`Aylananing uzunligi: ${L}`);
-
-/*
-let a = prompt("kvadrat tomonining qiymatini kiriting");
-let S = a ** 2;
-console.log(S);
-.
-.
-.
-*/
 
 ////////////Begin-3//////
 /*
-let R = prompt("Radius qiymatini kiriting");
-let PI = 3.14;
-let L = 2 * R * PI;
-let S = PI * R ** 2;
-console.log(`Yuzi: ${S}`);
-console.log(`Uzunligi: ${L}`);
-.
-.
-.
+let a = prompt("a tomonning qiymatini kiriting");
+let V = a ** 3;
+let S = 6 * a ** 2;
+console.log(`Hajmi: ${V}`);
+console.log(`To'la sirti: ${S}`);
 */
 // let R = prompt("Radius qiymatini kiriting");
-
+/*
 ////////////Begin-4//////
-// let a = prompt("a_ning qiymatinini kiriting");
-// let b = prompt("b_ning qiymatinini kiriting");
-// let c = a * b;
-// let G = c ** 1 / 2;
-// console.log(`ikki sonning o'rta geometrigi: ${G}`);
-
+let a = prompt("a tomon qiymatini kiriting");
+let b = prompt("b tomon qiymatini kiriting");
+let c = prompt("c tomon qiymatini kiriting");
+let V = a * b * c;
+let S = 2 * (a * b + b * c + a * c);
+console.log(`Hajmi: ${V}`);
+console.log(`To'la sirti: ${S}`);
+*/
 ////////////Begin-5//////
-// let x = prompt("x_ning qiymatinini kiriting");
-// let y = 3 * x ** 6 - 6 * x ** 2 - 7;
-// console.log(y);
+/*
+let a = prompt("a_ning qiymatini kiriting");
+let b = prompt("b_ning qiymatini kiriting");
+let M = a / 2 + b / 2;
+console.log(`ikki sonning o'rta arifmetigi: ${M}`);
+*/
 ////////////Begin-6//////
-// let a = prompt("a_ning qiymatinini kiriting");
-// let b = prompt("b_ning qiymatinini kiriting");
-// let c = a;
-// a = b;
-// b = c;
-// console.log(`a = ${a}`);
-// console.log(`b = ${b}`);
+
+let a = prompt("a_ning qiymatini kiriting");
+let b = prompt("b_ning qiymatini kiriting");
+let c = Math.sqrt(a ** 2 + b ** 2);
+
+let P = a + b + c;
+console.log(`gepotenuza = ${c}`);
+console.log(`Perimetri = ${P}`);
 
 ////////////Begin-7//////
+/*
+let r1 = prompt("1-radius qiymatini kiriting");
+let r2 = prompt("2-radius qiymatini kiriting");
+let PI = Math.PI;
+let S1 = PI * r1;
+let S2 = PI * r2;
+let S = PI * (r1 - r2);
+console.log(`1-aylana yuzi: ${S1}`);
+console.log(`2-aylana yuzi: ${S2}`);
+console.log(`1- va 2- aylana yuzi ayirmasi: ${S}`);
+*/
 ////////////Begin-8//////
+/*
+let L = prompt("Aylananing uzunligini kiriting");
+let PI = 3.14;
+let R = (PI * 2) / L;
+let S = PI * R ** 2;
+console.log(`Aylananing radiusi: ${R}`);
+console.log(`Aylananing yuzasi: ${S}`);
+*/
 ////////////Begin-9//////
 ////////////Begin-10//////
+
+// let num = prompt("biror raqam kiriting");
+// let res = Number(num);
+// let t = "3";
+
+// let result = t - 2;
+
+// console.log(result);
